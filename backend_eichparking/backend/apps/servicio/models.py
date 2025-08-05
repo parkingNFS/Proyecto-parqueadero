fcb613e92a33928b7deb73f98a369905a981e42c
jose garcia <joseangelgarciaarias@jesusamigo.com.co>
2025-08-05 14:40:48 -0500

    0508

--- backend_eichparking/backend/apps/servicio/models.py
+++ backend_eichparking/backend/apps/servicio/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class servicio (models.Model):
 
-    nombre = models.TextField("Nombre")
     usuario_id = models.TextField("Usuario")
-    placa_vehiculo = models.TextField("Placa de vehiculo")
+    placa_automovil = models.TextField("Placa de vehiculo")
 
